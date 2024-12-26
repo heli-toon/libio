f6b2de82f57e1e161bdc7e9a9474405d38c99084
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2024-12-26 07:14:14 +0000

    Add simple dashboard page

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,12 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('index.html', title="Home")
+    return render_template('index.html', title="home")
 
 @main.route('/login')
 def login():
     return render_template('login.html', title="login")
+
+@main.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', title="dashboard")
