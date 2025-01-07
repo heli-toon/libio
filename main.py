d3c8c7d6f4e50b4e3126ff1520add28dbaaaba19
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2025-01-07 17:46:06 +0000

    Add features and call-to-action sections to index page; enhance CSS for improved styling and layout

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 from app import create_app
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-
-# import cloudinary
-# import cloudinary.uploader
-# import cloudinary.api
-
-# Cloudinary Configuration
-# cloudinary.config(
-#     cloud_name=os.getenv("CLOUD_NAME"),
-#     api_key=os.getenv("API_KEY"),
-#     api_secret=os.getenv("API_SECRET"),
-#     secure=True
-# )
-
 
 app = create_app()
-
 
 # Configuration
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
